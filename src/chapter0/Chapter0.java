9ced90d34f59dfc9ccf1440fe5854b515af53dcf
kinaseryota <kinaseryota@192.168.1.3>
2021-02-27 19:52:22 +0900

    [finish]Chapter0

---
+++ src/chapter0/Chapter0.java
@@ -0,0 +1,18 @@
+package chapter0;
+
+public class Chapter0 {
+
+		public static void main (String[] args) {
+			System.out.println("Hello World");
+			System.out.println("Java勉強中！！");
+			System.out.println("がんばるぞ！！");
+			System.out.println("34 + 34");
+			System.out.println(34 + 34);
+			int x;
+			x = 8;
+			System.out.println(x * x * 3.14);
+		}
+
+
+
+}
