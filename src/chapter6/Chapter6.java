d3cf3e7d8dea1ca640520d8f6488adb9d4bd1dcd
kinaseryota <kinaseryota@192.168.1.3>
2021-03-04 19:36:07 +0900

    [finish]Chapter6

---
+++ src/chapter6/Chapter6.java
@@ -0,0 +1,13 @@
+package chapter6;
+
+public class Chapter6 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 2;
+		int total = ChapterLogic.tasu(a, b);
+		int delta = ChapterLogic.hiku(a, b);
+		System.out.println("足すと" + total + "、引くと" + delta);
+	}
+
+
+}
---
+++ src/chapter6/ChapterLogic.java
@@ -0,0 +1,13 @@
+package chapter6;
+
+public class ChapterLogic {
+	
+	public static int tasu(int a, int b) {
+		return(a + b);
+	}
+	
+	public static int hiku(int a, int b) {
+		return(a - b);
+	}
+	
+}
