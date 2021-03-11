60f63d7e9982ff4ddb37c618ffbe7e6b2c1fcc23
kinaseryota <kinaseryota@192.168.1.3>
2021-03-11 17:39:17 +0900

    [finish]chapter7,8

--- src/chapter7/Main.java
+++ src/chapter7/Main.java
@@ -7,13 +7,26 @@ public class Main {
 		// 勇者のフィールドに初期値をセット
 		h.name = "リョウタ";  //Heroクラスのhのフィールドにnameを代入
 		h.hp = 100;
-		System.out.println("勇者" + h.name + "が誕生しました!!");
-		// 勇者のメソッドを呼び出す
-		h.sit(5);
-		h.slip();
-		h.sit(25);
-		h.run();
+		// お化けキノコの作成
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
 		
+		Matango m2 = new Matango();
+		m2.hp = 48;
+		m2.suffix = 'B';
+		
+		Cleric c = new Cleric();
+		c.name = "あいう";
+		
+		System.out.println("勇者" + h.name + "が誕生しました!!");
+		// メソッドを呼び出す
+		h.slip();
+		m1.run();
+		m2.run();
+		h.run();
+		c.selfAid();
+		c.pray(10);
 		
 	}
 
--- src/chapter7/Matango.java
+++ src/chapter7/Matango.java
@@ -3,5 +3,9 @@ package chapter7;
 public class Matango {
 	int hp;
 	final int LEVEL = 10;  //定数で宣言（final..大文字）
+	char suffix;
+	public void run() {
+		System.out.println("お化けキノコ" + this.suffix + "は逃げ出した!");
+	}
 
 }
