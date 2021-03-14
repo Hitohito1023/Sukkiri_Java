f794c36e4b08008c3931f517f55b4b6e43851b34
kinaseryota <kinaseryota@192.168.1.3>
2021-03-14 17:38:45 +0900

    [finish]chapter13-3

---
+++ src/chapter13/Inn.java
@@ -0,0 +1,8 @@
+package chapter13;
+
+public class Inn {
+	public void checkIn(Hero h) {
+//		h.hp -= 100;
+	}
+
+}
---
+++ src/chapter13/King.java
@@ -0,0 +1,11 @@
+package chapter13;
+
+public class King {
+	void talk(Hero h) {
+		System.out.println("王様：ようこそ我が国へ、勇者" + h.getName() + "よ");
+		System.out.println("王様：長旅疲れたであろう。");
+		System.out.println("王様：まずは城下町をみてくると良い。ではまた会おう。");
+//		h.die();
+	}
+
+}
