c42fc108ff7c9305cbba8d30635317547b7125d6
kinaseryota <kinaseryota@192.168.1.3>
2021-03-15 15:48:00 +0900

    [finish]chapter15-4

--- src/chapter15/Main2.java
+++ src/chapter15/Main2.java
@@ -2,11 +2,22 @@ package chapter15;
 
 public class Main2 {
 	public static void main(String[] args) {
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < 1000; i++) {
-			sb.append("Java");
-		}
-		String s = sb.toString();
+//		StringBuilder sb = new StringBuilder();
+//		for(int i = 0; i < 1000; i++) {
+//			sb.append("Java");
+//		}
+//		String s = sb.toString();
+		
+//		String s = "abc,def:ghi";
+//		String[] words = s.split("[,:]");
+//		System.out.println(Arrays.toString(words));
+//		for (String w : words) {
+//			System.out.print(w + "->");
+//		}
+		
+		String s = "abc,def:ghi";
+		String w = s.replaceAll("[beh]", "x");
+		System.out.println(w);
 	}
 
 }
---
+++ src/chapter15/Valid.java
@@ -0,0 +1,9 @@
+package chapter15;
+
+public class Valid {
+	public static void main(String[] args) {
+		String s = "Java";
+		System.out.println(s.matches("Java"));
+	}
+
+}
