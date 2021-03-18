89df365a53a162ea9c5a5042e0fc0e9866cf26f4
kinaseryota <kinaseryota@192.168.1.3>
2021-03-18 15:11:17 +0900

    [finish]Chapter16

---
+++ src/chapter16/Main5.java
@@ -0,0 +1,21 @@
+package chapter16;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Hero {
+	public String name;
+}
+
+public class Main5 {
+	public static void main(String[] args) {
+		Hero h = new Hero();
+		h.name = "リョウタ";
+		List<Hero> list = new ArrayList<Hero>();
+		list.add(h);
+		h.name = "スガワラ";
+		System.out.println(list.get(0).name);
+	}
+	
+
+}
