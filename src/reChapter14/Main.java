082a705404e18ea8483c63cfe32457e12b107079
kinaseryota <kinaseryota@192.168.1.3>
2021-03-26 17:49:30 +0900

    [finish]re-chapter14

--- src/reChapter14/Main.java
+++ src/reChapter14/Main.java
@@ -1,5 +1,21 @@
 package reChapter14;
 
 public class Main {
+	public static void main(String[] args) {
+//		Hero h1 = new Hero();
+//		Hero h2 = new Hero();
+//		h1.hp = 100;
+//		h2.hp = 200;
+//		Hero.money = 2000;
+		
+		Hero.setRandomMoney();
+
+//		System.out.println(h1.hp);
+//		System.out.println(h2.hp);
+		System.out.println(Hero.money);
+//		System.out.println(h1.money);
+		
+		
+	}
 
 }
