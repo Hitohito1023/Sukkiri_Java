be7ae2e656b5ff0971f71f1375d61277e08027d4
kinaseryota <kinaseryota@192.168.1.3>
2021-03-26 18:43:45 +0900

    [stop]re-chapter15

--- src/reChapter15/Main.java
+++ src/reChapter15/Main.java
@@ -16,6 +16,30 @@ public class Main {
 		int[] array = {1, 2, 4, 5, 6};
 
 		System.out.println(array.length);
+
+		main1();
+		
+		main2();
+
+
+	}
+
+	public static void main1() {
+		String s4 = "Java and JavaScript";
+		if(s4.contains("Java")) {
+			System.out.println("文字列s4は、Javaを含んでいます");
+		}
+		if(s4.endsWith("Java")) {
+			System.out.println("文字列s4は。Javaが末尾にあります");
+		}
+		System.out.println("文字列s4で最初にJavaが登場する位置は" + s4.indexOf("Java"));
+		System.out.println("文字列s4で最後にJavaが登場する位置は" + s4.lastIndexOf("Java"));
+	}
+	
+	public static void main2() {
+		String s5 = "Java programming";
+		System.out.println("文字列s5の4文字目以降は" + s5.substring(3));
+		System.out.println("文字列s5の4~8文字目は" + s5.substring(3, 8));
 	}
 
 }
