d0e96f8a05eb558099ab621b7384fcf701d55d24
kinaseryota <kinaseryota@192.168.1.3>
2021-03-26 18:17:23 +0900

    [start]re-chapter15

---
+++ src/reChapter15/Main.java
@@ -0,0 +1,21 @@
+package reChapter15;
+
+public class Main {
+	public static void main(String[] args) {
+		String s1 = "スッキリJava";
+		String s2 = "Java";
+		String s3 = "java";
+		if(s2.equals(s3)) {
+			System.out.println("s2とs３は等しい");
+		}
+		if(s2.equalsIgnoreCase(s3)) {
+			System.out.println("s2とs3はケースを区別しなければ等しい");
+		}
+		System.out.println("s1の長さは" + s1.length() + "です");
+
+		int[] array = {1, 2, 4, 5, 6};
+
+		System.out.println(array.length);
+	}
+
+}
