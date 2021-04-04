a51f17f1314af34a456954c0df812a826b69dc71
Hitohito1023 <ryotakinase1023@gmail.com>
2021-04-04 15:52:11 +0900

    [start]re-chapter16

---
+++ src/reChapter16/Main.java
@@ -0,0 +1,29 @@
+package reChapter16;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+//		ArrayList<String> names = new ArrayList<String>();
+//
+//		names.add("ryota");
+//		names.add("taro");
+//		names.add("takeshi");
+//
+//		System.out.println(names.get(0));
+//		System.out.println(names.get(2));
+
+		ArrayList<Integer> points = new ArrayList<Integer>();
+		points.add(20);
+		points.add(100);
+		points.add(25);
+		for (int i : points) {
+			System.out.println(i);
+		}
+
+	}
+
+}
